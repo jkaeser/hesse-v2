c59dfc61609b529314659c4c76e302e74f84c53e
John Kaeser <jakaeser44@gmail.com>
2020-03-01 13:26:14 -0500

    Improve Button props structure.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import PropTypes from "prop-types"
+import Link from "gatsby-link"
 import './button.css'
 
-const Button = ({ children, path }) => (
-  <a href={ path } className="button">
-    { children }
-  </a>
-)
+const Button = ({ children, path, className }) => {
+  const classes = [
+    `button`,
+    className
+  ].join(' ');
+
+  // The Link component does not work well for external resources or files.
+  if (
+    path.substring(0, 4) === 'http' ||
+    path.substring(0, 7) === '/static'
+  ) {
+    return (
+      <a href={ path } className={ classes }>
+        { children }
+      </a>
+    )
+  }
+  else {
+    return (
+      <Link to={ path } className={ classes }>
+        { children }
+      </Link>
+    )
+  }
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  path: PropTypes.string.isRequired
+}
 
 export default Button
