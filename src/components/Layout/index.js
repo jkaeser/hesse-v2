127c307d55285a10484f386314cc408fa388e18f
John Kaeser <jakaeser44@gmail.com>
2020-03-06 15:24:06 -0500

    Set color state.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,20 +3,44 @@ import PropTypes from "prop-types"
 
 import "./layout.css"
 
-const Layout = ({ children, cols, attribute }) => {
-  const classes = [
-    'cols cols--' + cols,
-    attribute ? attribute : '',
-  ]
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorMode: 'default'
+    };
+    this.switchMode = this.switchMode.bind(this);
+  }
 
-  return (
-    <>
-      <main className={classes.join(' ')}>
-        {children}
-      </main>
-      <footer>© {new Date().getFullYear()}</footer>
-    </>
-  )
+  switchMode() {
+    if (this.state.colorMode === 'default') {
+      this.setState(prevState => ({
+        colorMode: 'contrast',
+      }))
+    }
+    else {
+      this.setState(prevState => ({
+        colorMode: 'default',
+      }))
+    }
+  }
+
+  render() {
+    const classes = [
+      'cols cols--' + this.props.cols,
+      this.props.attribute ? this.props.attribute : '',
+    ]
+
+    return (
+      <div className={`site-wrapper ${this.state.colorMode}`}>
+        <main className={classes.join(' ')}>
+          {this.props.children}
+        </main>
+        <button onClick={this.switchMode}>Switch Color Mode</button>
+        <footer>© {new Date().getFullYear()}</footer>
+      </div>
+    )
+  }
 }
 
 Layout.propTypes = {
