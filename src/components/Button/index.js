13f20acbc6959dff3784b79fc4a7bf740f4fcb6c
John Kaeser <jakaeser44@gmail.com>
2020-03-11 15:20:53 -0400

    Standardize button styles.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from "prop-types"
 import Link from "gatsby-link"
-import './button.css'
 
 const Button = ({ children, path, className }) => {
   const classes = [
