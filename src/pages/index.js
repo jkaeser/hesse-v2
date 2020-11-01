5bdb61f23c3cd562518d8f5aa63954e2ec3c9ff8
John Kaeser <jakaeser44@gmail.com>
2020-11-01 14:46:10 -0500

    Update home page copy.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
       I'm a web developer in Philadelphia, PA, currently working at <a href="https://www.zivtech.com/">Zivtech</a>.
     </p>
     <p>
-      Lately I've been rebuilding <a href="https://www.ias.edu/">The Institute for Advanced Studies'</a> website in Drupal 8 after supporting their current site for over two and a half years. Previously, I led the front end development of <a href="https://us.fertility.com/">Fertility.com</a>. Some other sites I've worked on include <a href="https://vaccinemakers.org/">The Vaccine Makers Project</a>, <a href="https://www.lsac.org/">The Law School Admissions Council</a>, and <a href="https://clarifi.org/">Clarifi</a>.
+      Most recently I rebuilt <a href="https://www.ias.edu/">The Institute for Advanced Studies'</a> website in Drupal 8 after supporting their previous site for over two and a half years. Previously, I led the front end development of <a href="https://us.fertility.com/">Fertility.com</a>. Some other sites I've worked on heavily include <a href="https://vaccinemakers.org/">The Vaccine Makers Project</a>, <a href="https://www.lsac.org/">The Law School Admissions Council</a>, and <a href="https://clarifi.org/">Clarifi</a>.
     </p>
     <p>
       When I'm not building websites, you can find me playing the drums in a band called <a href="https://dwellermusic.com/">Dweller</a>. I might also be playing <a href="https://mtg.gamepedia.com/Commander_(format)">Commander</a>. Or eating ice cream.
