8831188144d7d62d26d8755e6b60581050e24624
John Kaeser <jakaeser44@gmail.com>
2025-03-26 11:10:37 -0400

    Remove Starfield functionality.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 
-import Starfield from "../Starfield"
 import Footer from "../Footer"
 
 import "./styles/all.scss"
